bd045fca4d6196da8a689cc8cecea6dd3e016122
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-07 15:49:07 +0900

    first commit

---
+++ IncrementalSearch/IncrementalSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncrementalSearchApp.swift
+//  IncrementalSearch
+//
+//  Created by cmStudent on 2021/11/07.
+//
+
+import SwiftUI
+
+@main
+struct IncrementalSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
